09629baec68ba8b332d9fa3f6dd6d42320bbd51e
Colin Kennedy <colinfkennedy@gmail.com>
2019-12-03 00:13:59 +0000

    Day two solution

--- lib/main.rb
+++ lib/main.rb
@@ -1,22 +1,34 @@
 require_relative "read"
-require_relative "day_one"
+#require_relative "day_one"
+#
+#puts "Day One"
+#
+#input_day_one = Read.get_input(filename: "input/day-one-input.txt").map(&:to_i)
+#
+#total_fuel = []
+#input_day_one.each do |mass|
+#  total_fuel.push(DayOne.calculate_fuel(mass: mass))
+#end
+#
+#puts "Total fuel array: #{total_fuel}"
+#puts "Total fuel: #{total_fuel.sum}"
+#
+#total_fuel_array = []
+#input_day_one.each do |mass|
+#  total_fuel_array.push(DayOne.calculate_fuel_double(mass: mass))
+#end
+#
+#puts "Total fuel double array: #{total_fuel_array}"
+#puts "Total fuel double: #{total_fuel_array.sum}"
+#
 
-puts "Day One"
+require_relative "day_two"
 
-input_day_one = Read.get_input(filename: "input/day-one-input.txt").map(&:to_i)
+puts "Day Two"
 
-total_fuel = []
-input_day_one.each do |mass|
-  total_fuel.push(DayOne.calculate_fuel(mass: mass))
-end
+input_day_two = Read.get_input(filename: "input/day-two-input.txt")
+puts "Day two input:"
+puts input_day_two
+#DayTwo.process(input: input_day_two.first.split(',').map(&:to_i))
 
-puts "Total fuel array: #{total_fuel}"
-puts "Total fuel: #{total_fuel.sum}"
-
-total_fuel_array = []
-input_day_one.each do |mass|
-  total_fuel_array.push(DayOne.calculate_fuel_double(mass: mass))
-end
-
-puts "Total fuel double array: #{total_fuel_array}"
-puts "Total fuel double: #{total_fuel_array.sum}"
+DayTwo.process_two(input: input_day_two.first.split(',').map(&:to_i))
