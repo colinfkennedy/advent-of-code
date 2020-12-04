60c06b76b25fb230dd9c6bb78438d1843df91653
Colin Kennedy <colin.kennedy@intercom.io>
2020-12-04 08:28:50 +0000

    Day three solved

---
+++ lib/twenty_twenty/day_three.rb
@@ -0,0 +1,30 @@
+module TwentyTwenty
+  class DayThree
+    def self.number_of_trees(input, rotation_amount = 3)
+      rotation = 0
+      tree_count = 0
+      input.each do |line|
+        # puts "Line: #{line}"
+        rotated = line.strip.split(//).rotate(rotation)
+        # puts "Rotated: #{rotated}"
+        if rotated.first === "#"
+          tree_count += 1
+        end
+        rotation += rotation_amount
+      end
+      tree_count
+    end
+
+    def self.product_of_slopes(input)
+      slope_one_count = self.number_of_trees(input, 1)
+      slope_two_count = self.number_of_trees(input, 3)
+      slope_three_count = self.number_of_trees(input, 5)
+      slope_four_count = self.number_of_trees(input, 7)
+      input_every_second = (0...input.size).step(2).map { |i| input[i] }
+      # puts input_every_second
+      slope_five_count = self.number_of_trees(input_every_second, 1)
+      puts "#{slope_one_count} * #{slope_two_count} * #{slope_three_count} * #{slope_four_count} * #{slope_five_count}"
+      slope_one_count * slope_two_count * slope_three_count * slope_four_count * slope_five_count
+    end
+  end
+end
--- lib/twenty_twenty/main.rb
+++ lib/twenty_twenty/main.rb
@@ -1,6 +1,7 @@
 require_relative "../read"
 # require_relative "day_one"
-require_relative "day_two"
+# require_relative "day_two"
+require_relative "day_three"
 
 # puts "Day One - 2020"
 #
@@ -10,10 +11,18 @@ require_relative "day_two"
 #
 # puts "Product of 3: #{::TwentyTwenty::DayOne.product_3_sum_2020(input_day_one)}"
 
-puts "Day Two - 2020"
-
-input_day_two = Read.get_input(filename: "../input/2020/day-two.txt")
+# puts "Day Two - 2020"
+#
+# input_day_two = Read.get_input(filename: "../input/2020/day-two.txt")
 
 # puts "Correct passwords range: #{TwentyTwenty::DayTwo.number_correct_passwords_range(input_day_two)}"
 
-puts "Correct passwords position: #{TwentyTwenty::DayTwo.number_correct_passwords_position(input_day_two)}"
+# puts "Correct passwords position: #{TwentyTwenty::DayTwo.number_correct_passwords_position(input_day_two)}"
+
+puts "Day Three - 2020"
+
+input_day_three = Read.get_input(filename: "../input/2020/day-three.txt")
+
+# puts "Tree count: #{TwentyTwenty::DayThree.number_of_trees(input_day_three)}"
+
+puts "Product of slopes: #{TwentyTwenty::DayThree.product_of_slopes(input_day_three)}"
